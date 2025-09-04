9ba323380c577cb4a0812b54270049ab26025d4a
YoedSoka <awesomeyoed@gmail.com>
2025-09-04 11:38:34 -0600

    Operacion Suma

--- src/main/java/mx/edu/utez/Menu.java
+++ src/main/java/mx/edu/utez/Menu.java
@@ -1,4 +1,46 @@
 package mx.edu.utez;
+import java.util.Scanner;
 
 public class Menu {
-}
+    private int a, b, res;
+
+    public int getA() {
+        return a;
+    }
+    public void setA(int a) {
+        this.a = a;
+    }
+
+    public int getB() {
+        return b;
+    }
+
+    public void setB(int b) {
+        this.b = b;
+    }
+
+    public int getRes() {
+        return res;
+    }
+
+    public void setRes(int res) {
+        this.res = res;
+    }
+
+    public Menu() {}
+
+    public void sumar () {
+        Scanner sc = new Scanner(System.in);
+        int v;
+        System.out.println("Ingresa el primer numero para sumar: ");
+        setA(sc.nextInt());
+        sc.nextLine();
+
+        System.out.println("Ingresa el segundo numero para sumar: ");
+        setB(sc.nextInt());
+        sc.nextLine();
+
+        v = a + b;
+        setRes(v);
+        System.out.println(getA() + " + " + getB() + " = " + getRes());
+    }
